e8deb0e48bf786ef4238ab98fc1da39380b9f0aa
nui-dev <contact@nuirattapon.com>
2021-01-30 23:43:27 +1300

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true
 };
+
+// Initialize Firebase
+export const firebaseconfig = {
+  apiKey: "AIzaSyDa9QKxbLE_MqZas7_8ObDSfL4ok30t-C4",
+  authDomain: "nuiweb-69916.firebaseapp.com",
+  databaseURL: "https://nuiweb-69916.firebaseio.com",
+  projectId: "nuiweb-69916",
+  storageBucket: "nuiweb-69916.appspot.com",
+  messagingSenderId: "1098485121444"
+};
